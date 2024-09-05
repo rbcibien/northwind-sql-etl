ce086f25b86317e2df80a86de5f17c0637c5b52e
Rômulo Cibien <romulo.cibien@hotmail.com.br>
2024-09-04 22:45:25 -0300

    feat: add new sql query about materialized view and triggers

---
+++ deasafio-aula-09.sql
@@ -0,0 +1,46 @@
+-- Materialized view
+CREATE MATERIALIZED VIEW sales_accumulated_monthly_mv AS
+    SELECT
+        EXTRACT(YEAR FROM o.order_date) AS year,
+        EXTRACT(MONTH FROM o.order_date) AS month,
+        SUM(od.unit_price * od.quantity * (1 - od.discount)) AS monthly_sales,
+        ROUND(
+            CAST(
+                SUM(
+                    (od.unit_price * od.quantity)  * (1.0 - od.discount)
+                ) OVER (
+                    PARTITION BY EXTRACT(YEAR FROM o.order_date)
+                    ORDER BY EXTRACT(MONTH FROM o.order_date)
+                )
+                AS numeric
+            ),
+            2
+        ) AS sales_ytd
+    FROM
+        orders o
+        INNER JOIN order_details od ON od.order_id = o.order_id
+    GROUP BY
+        EXTRACT(YEAR FROM o.order_date),
+        EXTRACT(MONTH FROM o.order_date)
+    ORDER BY
+        1, 2;
+
+-- Triggers
+CREATE OR REPLACE FUNCTION refresh_sales_accumulated_monthly_mv() RETURNS TRIGGER AS $$
+    BEGIN
+        REFRESH MATERIALIZED VIEW sales_accumulated_monthly_mv;
+        RETURN NULL;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER trg_refresh_sales_accumulated_monthly_mv_order_details
+    AFTER INSERT OR UPDATE OR DELETE ON order_details
+    FOR EACH STATEMENT
+    EXECUTE refresh_sales_accumulated_monthly_mv();
+
+CREATE OR REPLACE TRIGGER trg_refresh_sales_accumulated_monthly_mv_orderS
+    AFTER INSERT OR UPDATE OR DELETE ON order
+    FOR EACH STATEMENT
+    EXECUTE refresh_sales_accumulated_monthly_mv();
+
+-- Stored Procedures
